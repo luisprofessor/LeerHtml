347e770e0f32ab507ba67cfef87a4650c3136d56
luisprofessor <luisprofessor@gmail.com>
2019-10-16 10:15:26 -0300

    actualizado

--- app/src/main/java/com/example/leerhtml/MainActivity.java
+++ app/src/main/java/com/example/leerhtml/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.widget.Toast;
 
 import org.jsoup.Jsoup;
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 Elements mElementDataSize = mBlogDocument.select("img");
                 // Locate the content attribute
                 int mElementSize = mElementDataSize.size();
-                int posi=mElementDataSize.indexOf("nook.png");
+
                 atributo=mElementDataSize.attr("src");
 
             } catch (IOException e) {
@@ -56,13 +56,16 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(Void result) {
             // Set description into TextView
 
-            if(atributo.equals("nook.png")){
-                Toast.makeText(getApplicationContext(),"no aceptado",Toast.LENGTH_LONG).show();
-            }else if(atributo.equals("pone el otro png")){
-                Toast.makeText(getApplicationContext(),"no aceptado",Toast.LENGTH_LONG).show();
+            if(atributo!=null) {
+                if (atributo.equals("nook.png")) {
+                    Toast.makeText(getApplicationContext(), "no aceptado", Toast.LENGTH_LONG).show();
+                } else if (atributo.equals("pone el otro png")) {
+                    Toast.makeText(getApplicationContext(), "no aceptado", Toast.LENGTH_LONG).show();
+                }
+
+            }else {
+                Toast.makeText(getApplicationContext(), "no puedo acceder al servidor", Toast.LENGTH_LONG).show();
             }
-
-
 
         }
     }
